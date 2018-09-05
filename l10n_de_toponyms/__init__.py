207a6f35e4cc1307fab16670f65a98e65e14bd1a
fkantelberg <florian.kantelberg@initos.com>
2018-09-05 14:24:50 +0200

    Creating of l10n_de_toponyms module

---
+++ l10n_de_toponyms/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2018 Dipak Suthar <dipaks@itis.de>
+# Copyright 2018 Florian Kantelberg <florian.kantelberg@initos.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
